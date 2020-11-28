318d46484124a6a681bc949adcc14b97394b65cf
Yassin Siouda <yassin@siouda.com>
2020-11-28 19:01:24 +0100

    Update extract_img_from_video.sh

--- extract_img_from_video.sh
+++ extract_img_from_video.sh
@@ -5,6 +5,6 @@ do
   filename="$(basename -- $f)"
   echo "Processing $f file..."
   # take action on each file. $f store current file name
-  ffmpeg -i $f  -vf fps=1/4  output/image_extract/$filename-%d.jpg
+  ffmpeg -i $f  -vf fps=1/4 -r 1 -q:v 1 -qmin 1 -qmax 1 output/image_extract/$filename-%d.jpg
 done
 
